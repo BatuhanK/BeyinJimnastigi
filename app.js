ef324958e3655c455240509dd5b51b7fbc435b92
BatuhanK <katircib@gmail.com>
2014-11-24 15:45:52 +0200

    zaman işlemleri
    kullanıcının ne kadar sürede cevap verdiğini cevap olarak atmaya
    başladık, veritabanı vs. aşamalara geçtiğimizde bu veriler kullanılıp
    puanlama yapılacak

--- app.js
+++ app.js
@@ -10,12 +10,26 @@ var T = new Twit({
 })
 
 
+var time_format = function(seconds){
+    var sec_num = parseInt(seconds, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds;
+    return time;
+}
+
 
 // Application logic
 
 var error_count = 0;
 var current_answer;
 var current_question;
+var last_time;
 var create_question = function() {
     var number_one   = ((Math.floor(Math.random() * 15000) + 1) % 200)+1;
     var number_two   = ((Math.floor(Math.random() * 15000) + 1) % 100)+1;
@@ -52,6 +66,8 @@ var create_question = function() {
 
     current_question = question;
     current_answer   = eval(question);
+    last_time        = new Date();
+
     return question;
 }
  
@@ -71,8 +87,20 @@ var send_question = function() {
 var correct_answer = function(tweet) {
     var sender = tweet.user.screen_name;
     var tweet_id = tweet.id_str;
-    var reply_tweet = util.format("Tebrikler @%s doğru cevap verdin ! (%s) = %s",sender,current_question,current_answer);
+    // Zaman işlemleri
+    time = tweet.created_at;
+    time = time.replace(/\.\d+/, ""); // remove milliseconds
+    time = time.replace(/-/, "/").replace(/-/, "/");
+    time = time.replace(/T/, " ").replace(/Z/, " UTC");
+    time = time.replace(/([\+\-]\d\d)\:?(\d\d)/, " $1$2"); // -04:00 -> -0400
+    time = new Date(time * 1000 || time);
 
+    var seconds = (( time-last_time.getTime() ) * .001) >> 0;
+    var time_diff = time_format(seconds);
+
+
+    // Cevap verelim
+    var reply_tweet = util.format("Tebrikler @%s doğru cevap verdin ! (%s) = %s | Süre : %s",sender,current_question,current_answer,time_diff);
     
     T.post('statuses/update', { in_reply_to_status_id : tweet_id, status:reply_tweet}, function(err, data, response) {
         if(err) console.log("Tweet gönderilirken bir hata oluştu !");
